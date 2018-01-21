188d7f4d87bad7150011c5e2394f663d1e6d3438
Sarp Basaraner <sarpbasaraner@gmail.com>
2018-01-21 13:19:11 +0300

    parse text file

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
-extern crate pancurses;
-
-use pancurses::{initscr, endwin, Input, noecho};
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+use std::error::Error;
 
 fn main() {
-  let window = initscr();
-  window.printw("Type things, press delete to quit\n");
-  window.refresh();
-  window.keypad(true);
-  noecho();
-  loop {
-      match window.getch() {
-          Some(Input::Character(c)) => { window.addch(c); },
-          Some(Input::KeyDC) => break,
-          Some(input) => { window.addstr(&format!("{:?}", input)); },
-          None => ()
-      }
-  }
-  endwin();
+	// path to file
+	let path = Path::new("lib/commoncap.txt");
+
+	// open it in read-only mode
+	let mut file = match File::open(&path) {
+		Err(why) => panic!("couldn't open: {}", why.description()),
+		Ok(file) => file,
+	};
+
+	// write to string
+	let mut file_content = String::new();
+	match file.read_to_string(&mut file_content) {
+		Err(why) => panic!("couldn't read: {}", why.description()),
+		Ok(_) => (),
+	}
+
+	println!("file: {}", file_content);
 }
